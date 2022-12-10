d49abdb3c7c7c7ac543f371f8c830c168112e191
Vinoth's GIT <71130419+vinothkadir@users.noreply.github.com>
2022-12-10 22:51:27 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:Vinoth9591asdqaz@localhost:5432/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:****@localhost:5432/fastapi"
 
 #SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
